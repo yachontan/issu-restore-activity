93ad84b04b9d5f3adbd7eb85557657474f7454f7
yachontan <62540162+yachontan@users.noreply.github.com>
2021-08-23 13:16:30 +0900

    Update read_file.py
    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -4,7 +4,6 @@ import csv
 import sys
 import datetime
 
-
 def csv_read(csv_contents):
 ## hostip -
 ## port - telnet tcp port 
@@ -12,15 +11,28 @@ def csv_read(csv_contents):
 ## password
 # open file
     csv_file = open(csv_contents, "r", encoding="ms932", errors="", newline="" )
+#    csv_file = open('Telnet-list.csv', "r", encoding="ms932", errors="", newline="" )
     f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
     csv_list = []
+    #Split up header and data
+    #eader
+    header = next(f)
+    #
     csv_list = [row for row in f]
     return csv_list
  
 
 def txt_read(txt_contents):
-   txt_list = []
-   f = open(txt_contents)
-   txt_list = f.readlines()
-   f.close()
-   return txt_list
+    txt_list = []
+    f = open(txt_contents)
+    for row in f:
+     txt_list = f.read().splitlines()
+    f.close()
+#    for i in range(len(f2)):
+#        txt_list[i] =f2[i].replace('\n','')
+    return txt_list
+
+
+
+#telnet_list = csv_read("Telnet-list.csv")
+#print(telnet_list)
