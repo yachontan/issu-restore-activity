9b90ce19c7e8aff31c953d710416afc773875a13
yachontan <62540162+yachontan@users.noreply.github.com>
2021-08-23 21:22:06 +0900

    Update to add telnet method in maincode.py

--- maincode.py
+++ maincode.py
@@ -6,31 +6,34 @@ import telnetlib
 import read_file
 import dotelnet
 
-#Check csv file is exist
+# Check csv file is exist
 
-#read csv and put into host_list[]
+# Read csv and put into host_list[]
 lineinput = input("Input Telnet list file .csv:")
 telnet_list = read_file.csv_read(lineinput)
 print(telnet_list)
 
+# set telnet list to each parameter
+for i in range(len(telnet_list)):
+  #defile param from telnet_list
+  ip = telnet_list[i][0]
+  port = telnet_list[i][1]
+  Username = telnet_list[i][2]
+  tel_password = telnet_list[i][3]
+  en_password = telnet_list[i][4]
+  config_list = telnet_list[i][5]
+  show_list = telnet_list[i][6]
+  target_ios = telnet_list[i][7]
+  tftp_server = telnet_list[i][8]
+  #Connect telnet
+  tn = telnetlib.Telnet(ip,port,600)
+  print(tn)
+  tn.interact()
+  
+  # Close telnet
+  tn.close()
 
-lineinput = "show.txt"
-show_list = read_file.txt_read(lineinput)
-print(show_list)
-
-#telnet from i of host_list
-f = []
-#for i in range(len(csv_list)):
-#for i in range(len(csv_list)) :
-#  if i > 1:
-#    print(i)
-#    dotelnet.main(i[0],i[1],i[2],i[3],i[4])
-    
-    
-#    tn = telnetlib.Telnet(i[0], i[1])
-
-# prompt.py
-
-# <message>
-
-# 
+# Example to make a list[] from txt
+# lineinput = "show.txt"
+# show_list = read_file.txt_read(lineinput)
+# print(show_list)
