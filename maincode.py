5da15f64d137a874051413c7742a24640cdf695a
Yachon Tang <yachontan@gmail.com>
2021-08-24 17:04:48 +0900

    update files

--- maincode.py
+++ maincode.py
@@ -5,32 +5,40 @@ import datetime
 import telnetlib
 import read_file
 import dotelnet
+#import prompt
 
-#Check csv file is exist
+# Check csv file is exist
 
-#read csv and put into host_list[]
+# Read csv and put into host_list[]
 lineinput = input("Input Telnet list file .csv:")
 telnet_list = read_file.csv_read(lineinput)
 print(telnet_list)
 
+# set telnet list to each parameter
+for i in range(len(telnet_list)):
+  #defile param from telnet_list
+  ip = telnet_list[i][0]
+  port = telnet_list[i][1]
+  Username = telnet_list[i][2]
+  tel_password = telnet_list[i][3]
+  en_password = telnet_list[i][4]
+  config_list = telnet_list[i][5]
+  show_list = telnet_list[i][6]
+  target_ios = telnet_list[i][7]
+  tftp_server = telnet_list[i][8]
+  print(ip)
+  print(port)
+  print(tftp_server)
 
-lineinput = "show.txt"
-show_list = read_file.txt_read(lineinput)
-print(show_list)
+  #Connect telnet
+  #parameter = prompt. 
+  #tn = telnetlib.Telnet(ip,port,600)
+  #print(tn)
+  #tn.interact()
+  
+  # Close telnet
+  #tn.close()
 
-#telnet from i of host_list
-f = []
-#for i in range(len(csv_list)):
-#for i in range(len(csv_list)) :
-#  if i > 1:
-#    print(i)
-#    dotelnet.main(i[0],i[1],i[2],i[3],i[4])
-    
-    
-#    tn = telnetlib.Telnet(i[0], i[1])
-
-# prompt.py
-
-# <message>
-
-# 
+# Example to make a list[] from txt
+parameter = read_file.txt_read(show_list)
+print(parameter)
--- read_file.py
+++ read_file.py
@@ -5,19 +5,13 @@ import sys
 import datetime
 
 def csv_read(csv_contents):
-## hostip -
-## port - telnet tcp port 
-## username - 
-## password
-# open file
     csv_file = open(csv_contents, "r", encoding="ms932", errors="", newline="" )
-#    csv_file = open('Telnet-list.csv', "r", encoding="ms932", errors="", newline="" )
     f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
     csv_list = []
     #Split up header and data
-    #eader
+    #skip header
     header = next(f)
-    #
+    #input csv_list from next(f)
     csv_list = [row for row in f]
     return csv_list
  
@@ -28,11 +22,7 @@ def txt_read(txt_contents):
     for row in f:
      txt_list = f.read().splitlines()
     f.close()
-#    for i in range(len(f2)):
-#        txt_list[i] =f2[i].replace('\n','')
     return txt_list
 
 
 
-#telnet_list = csv_read("Telnet-list.csv")
-#print(telnet_list)
