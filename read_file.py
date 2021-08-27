3272ee8b0cf053b93b55263851233143b6eea02f
Yachon Tang <yachontan@gmail.com>
2021-08-27 18:00:53 +0900

    fix txt_read in read_file.py

--- read_file.py
+++ read_file.py
@@ -4,6 +4,8 @@ import csv
 import sys
 import datetime
 
+csv_contents = "Telnet-list.csv"
+
 def csv_read(csv_contents):
     csv_file = open(csv_contents, "r", encoding="ms932", errors="", newline="" )
     f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
@@ -19,10 +21,7 @@ def csv_read(csv_contents):
 def txt_read(txt_contents):
     txt_list = []
     f = open(txt_contents)
-    for row in f:
-     txt_list = f.read().splitlines()
+    #for row in f:
+    txt_list = f.read().splitlines()
     f.close()
     return txt_list
-
-
-
