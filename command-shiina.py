2597362b00edc16c4ac4c02c86b1b8b84a5defb0
Yachon Tang <yachontan@gmail.com>
2021-09-15 17:08:24 +0900

    Update Command.py and make backup command-shiina.py

--- command-shiina.py
+++ command-shiina.py
@@ -3,16 +3,15 @@ import read_file
 import glossary
 
 def output_show(ip,port,show_list,tn):
-    log_name = glossary.set_telnet_log_name(ip,port,"show_result")
     
     print("Commands in '", show_list,"' will be issued.")
     command_list = read_file.txt_read(show_list)
     
     tn.write(b"\r\n")
     tn.write(b"\r\n")
-    glossary.telnet_log(log_name,tn.read_until(b"#").decode('ascii'))
+    tn.read_until(b"#")
     tn.write(b"terminal length 0\n")
-    glossary.telnet_log(log_name,tn.read_until(b"#").decode('ascii'))
+    tn.read_until(b"#")
     tn.write(b"\n")
 
     for com in command_list:
@@ -20,27 +19,23 @@ def output_show(ip,port,show_list,tn):
         tn.write(com.encode('ascii') + b"\n")
 
     tn.write(b"!!!!!! DONE !!!!!!" + b"\n")
-    glossary.telnet_log(log_name,tn.read_until(b"!!!!!! DONE !!!!!!").decode('ascii'))
-        
     tn.write(b"terminal default length\n")
-    #output = tn.read_until(b"!!!!!! DONE !!!!!!").decode('ascii')
-
+    output = tn.read_until(b"!!!!!! DONE !!!!!!").decode('ascii')
 
     tn.write(b"\n")
-    glossary.telnet_log(log_name,tn.read_until(b"#").decode('ascii'))
+    tn.read_until(b"#")
     
     # Make output file and write output in it
-    #dt_now = datetime.datetime.now()
-    #log_output = ip + "_" + port + "_" + dt_now.strftime('%Y%m%d-%H%M%S-%f') + "_" + "show_result.txt"
-    #f_log = open(log_output,mode="w", newline="", encoding="ms932")
-    #f_log.write("\n---- (telnet " + ip + " : " + port +") ----\n")
-    #f_log.write(output)
-    #f_log.close()
+    dt_now = datetime.datetime.now()
+    log_output = ip + "_" + port + "_" + dt_now.strftime('%Y%m%d-%H%M%S-%f') + "_" + "show_result.txt"
+    f_log = open(log_output,mode="w", newline="", encoding="ms932")
+    f_log.write("\n---- (telnet " + ip + " : " + port +") ----\n")
+    f_log.write(output)
+    f_log.close()
 
     print("... Done!")
-    #print("### ", log_output, "is created.")
-    print("### "+ log_name +"is created.")
-    
+    print("### ", log_output, "is created.")
+
 
 def input_conf(ip,port,config_list,tn):
     
